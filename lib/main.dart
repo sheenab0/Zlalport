e5c16100ba58b7e7fc2d8ac6179a99a3ad14336b
sheenab0 <oman89k6@gmail.com>
2025-06-30 21:36:30 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Al Bort App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
